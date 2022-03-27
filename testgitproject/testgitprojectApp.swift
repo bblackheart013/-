1ecd354153bb7562d31fe22c23d82c1b53bec151
MSF <mohdsarfaraz1306@icloud.com>
2022-03-27 10:42:06 +0530

    Initial Commit

---
+++ testgitproject/testgitprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitprojectApp.swift
+//  testgitproject
+//
+//  Created by MOHD SARFARAZ on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct testgitprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
